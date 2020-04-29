74a3fef7ee2faaabb9167cbf0c2922d4a156d4e1
boertel <ben@comediadesign.com>
2020-04-29 10:08:26 -0700

    resize only when it is smaller

--- client/src/ui/Editor.js
+++ client/src/ui/Editor.js
@@ -156,7 +156,7 @@ function Editor({ value, onChange, characters, onMetaEnter, ...props }) {
     // max height when resizing automatically but not when the user does it manually
     const maxHeight = 200
     const { scrollHeight } = ref.current
-    if (scrollHeight >= maxHeight) {
+    if (scrollHeight <= maxHeight) {
       ref.current.style.height = `5px`
       ref.current.style.height = `${ref.current.scrollHeight}px`
     }
